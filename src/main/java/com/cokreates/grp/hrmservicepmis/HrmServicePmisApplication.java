8256ea7edd7e443b4ae8d5de0a20ca90d56942c2
mashfiqur <m.rahman@cokreates.com>
2020-02-13 15:56:31 +0600

    [Initial Commit]

---
+++ src/main/java/com/cokreates/grp/hrmservicepmis/HrmServicePmisApplication.java
@@ -0,0 +1,13 @@
+package com.cokreates.grp.hrmservicepmis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmServicePmisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmServicePmisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cokreates/grp/hrmservicepmis/HrmServicePmisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cokreates.grp.hrmservicepmis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmServicePmisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
