2848fef88a76a6476088f52d874b221663084485
mashfiqur <m.rahman@cokreates.com>
2020-02-13 16:20:51 +0600

    [setup] primary setup compiatble with deployment script

--- src/main/java/com/cokreates/grp/hrmservicepmis/Application.java
+++ src/main/java/com/cokreates/grp/hrmservicepmis/Application.java
@@ -2,12 +2,13 @@ package com.cokreates.grp.hrmservicepmis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HrmServicePmisApplication {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrmServicePmisApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/cokreates/grp/hrmservicepmis/ApplicationTests.java
+++ src/test/java/com/cokreates/grp/hrmservicepmis/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HrmServicePmisApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
