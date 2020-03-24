2103eeeeb104689d61ae6ef130c358cb31adba6c
mrinalCoKreates <mrinaldebnath42@gmail.com>
2020-03-24 15:23:12 +0600

    all addresses redefined

--- src/main/java/com/cokreates/grp/beans/common/PersonalDTO.java
+++ src/main/java/com/cokreates/grp/beans/common/PersonalDTO.java
@@ -2,11 +2,12 @@ package com.cokreates.grp.beans.common;
 
 import com.cokreates.grp.beans.personal.bank.BankDTO;
 import com.cokreates.grp.beans.personal.childEducation.ChildEducationDTO;
-import com.cokreates.grp.beans.personal.emergencyContact.EmergencyContactDTO;
+import com.cokreates.grp.beans.personal.emergencyContactAddress.EmergencyContactAddress;
 import com.cokreates.grp.beans.personal.familyInfo.FamilyInfoDTO;
 import com.cokreates.grp.beans.personal.file.FileDTO;
 import com.cokreates.grp.beans.personal.general.GeneralDTO;
 import com.cokreates.grp.beans.personal.health.HealthDTO;
+import com.cokreates.grp.beans.personal.presentAddress.PresentAddressDTO;
 import com.cokreates.grp.beans.personal.sicknesses.InjuriesSicknessDTO;
 import com.cokreates.grp.beans.personal.travel.TravelDTO;
 import com.cokreates.grp.beans.qualification.education.EducationDTO;
@@ -17,12 +18,12 @@ public class PersonalDTO {
     List<BankDTO> bank;
     List<FileDTO> file;
     List<TravelDTO> travel;
-    List<com.cokreates.grp.beans.personal.address.PresentAddressDTO> address;
+    List<PresentAddressDTO> address;
     List<EducationDTO> education;
     List<FamilyInfoDTO> familyInfo;
     List<ChildEducationDTO> childEducation;
     List<InjuriesSicknessDTO> injuriesSickness;
     HealthDTO health;
     GeneralDTO general;
-    EmergencyContactDTO emergencyContact;
+    EmergencyContactAddress emergencyContact;
 }
