15933bfdf6ebe9c4b337cc583e1ca301db64ed1a
mrinalCoKreates <mrinaldebnath42@gmail.com>
2020-03-29 14:42:21 +0600

    employes added for being reviewed

--- src/main/java/com/cokreates/grp/util/dummyService/EmployeeOfficeService.java
+++ src/main/java/com/cokreates/grp/util/dummyService/EmployeeOfficeService.java
@@ -12,9 +12,7 @@ import java.util.Set;
 public class EmployeeOfficeService {
     public Set<String> getEmployeeByOffice(Set<String> officeOidList) {
 //        return new HashSet<>();
-        return new HashSet<>(Arrays.asList("a1042f1f-589f-4ce9-90f9-e248eb21c6f8",
-                             "0509ca00-5d86-4626-9265-ad902c228967",
-                             "7f0fb15c-1833-4143-b5b8-6d3bdc6b6ca8",
-                             "5c2adcf3-5108-4df5-8e89-0fbef976b159"));
+        return new HashSet<>(Arrays.asList("f1613e8b-bb23-414f-b82d-4973edc6f15a",
+                             "db74efdb-afe5-45d7-82da-43727378b755"));
     }
 }
