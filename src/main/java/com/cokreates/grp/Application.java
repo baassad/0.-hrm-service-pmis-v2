70b1cc759e229ddadfa1133d4b2087d75d698ec3
mrinalCoKreates <mrinaldebnath42@gmail.com>
2020-03-29 12:38:56 +0600

    ready for deployment

--- src/main/java/com/cokreates/grp/Application.java
+++ src/main/java/com/cokreates/grp/Application.java
@@ -3,13 +3,14 @@ package com.cokreates.grp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients(basePackages="com.cokreates")
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Application /*extends SpringBootServletInitializer */{
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
