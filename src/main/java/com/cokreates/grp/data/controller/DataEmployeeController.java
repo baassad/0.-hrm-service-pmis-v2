2248752a394eb0c4909097842c3a591ea6fff7ac
Shariar076 <shariar1405076@gmail.com>
2020-06-06 02:42:51 +0600

    [ongoing] node-in-emp-doc

--- src/main/java/com/cokreates/grp/data/controller/DataEmployeeController.java
+++ src/main/java/com/cokreates/grp/data/controller/DataEmployeeController.java
@@ -1,13 +1,17 @@
 package com.cokreates.grp.data.controller;
+import java.util.Map;
 
 import com.cokreates.grp.data.repository.DataCustomRepository;
 import com.cokreates.grp.data.service.DataEmployeeService;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
 
 @Controller
 public class DataEmployeeController {
@@ -23,5 +27,15 @@ public class DataEmployeeController {
     @ResponseBody
     public String getEmployee() {
         return repository.getAllEmployees();
+    }
+
+    @RequestMapping("hrm/pmis/get/v1/node-in-emp-doc")
+    @ResponseBody
+    public String getNodeFromEmployeeDoc(@RequestBody Map<String, Object> requestBody) {
+        JSONObject jsonBody = new JSONObject(requestBody).getJSONObject("body");
+        String employeeOid = jsonBody.getString("employeeOid");
+        JSONArray nodePath = jsonBody.getJSONArray("nodePath");
+        System.out.println(nodePath);
+        return repository.getEmployees(employeeOid);
     }
 }
--- src/main/java/com/cokreates/grp/data/repository/DataCustomRepository.java
+++ src/main/java/com/cokreates/grp/data/repository/DataCustomRepository.java
@@ -1,11 +1,15 @@
 package com.cokreates.grp.data.repository;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 @Repository
 public class DataCustomRepository {
@@ -18,5 +22,26 @@ public class DataCustomRepository {
         List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
         System.out.println(result.get(0));
         return result.toString();
+    }
+
+    public String getEmployees(String employeeOid) {
+        // String query="insert into employee values('"+e.getId()+"','"+e.getName()+"','"+e.getSalary()+"')";  
+        String query = "SELECT "
+        + "p.employee_main as employee_main, "
+        + "p.employee_temp as employee_temp "
+        + "FROM "
+        + "hrm.pmis p "
+        + "WHERE "
+        + "p.oid = '"
+        + employeeOid
+        + "'";
+        
+        // Map <String, Object> result = jdbcTemplate.queryForMap(query);
+        // Map <String, Object> employeeMain = (Map<String, Object>) result.get("employee_main");
+        // System.out.println(result.get("employee_main"));
+        
+        JSONObject result = new JSONObject(jdbcTemplate.queryForMap(query));
+        JSONObject employeeMain =  new JSONObject(result.get("employee_main"));
+        return employeeMain.toString();
     }
 }
