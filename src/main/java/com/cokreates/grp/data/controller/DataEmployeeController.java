5c1953d7db22a1ac3a4e36469ca22582063bc6bf
dv66 <mostofamonsur9396@gmail.com>
2020-06-08 12:38:45 +0600

    [refactor] generic type removed from controller

--- src/main/java/com/cokreates/grp/data/controller/DataEmployeeController.java
+++ src/main/java/com/cokreates/grp/data/controller/DataEmployeeController.java
@@ -32,7 +32,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
-public class DataEmployeeController<T> {
+public class DataEmployeeController {
 
     @Autowired
     DataEmployeeService dataEmployeeService;
