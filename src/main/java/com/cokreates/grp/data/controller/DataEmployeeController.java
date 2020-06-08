200ccd30d67ed88db28d8e6d5ec4a2db74f0105b
shariar076 <shariar1405076@gmail.com>
2020-06-08 13:15:09 +0600

    [refactor] node-in-emp-doc

--- src/main/java/com/cokreates/grp/data/controller/DataEmployeeController.java
+++ src/main/java/com/cokreates/grp/data/controller/DataEmployeeController.java
@@ -1,24 +1,16 @@
 package com.cokreates.grp.data.controller;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import com.cokreates.grp.data.constants.Api;
 import com.cokreates.grp.data.repository.DataCustomRepository;
 import com.cokreates.grp.data.service.DataEmployeeService;
 import com.cokreates.grp.data.util.DataUtil;
 import com.cokreates.grp.data.util.JsonUtil;
 
-import org.apache.tomcat.util.json.JSONParser;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,9 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
 
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -61,36 +51,19 @@ public class DataEmployeeController {
         return response;
     }
 
-    @RequestMapping(value = Api.READ_NODE_FROM_EMPLOYEE_DOC, method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<?>  getNodeFromEmployeeDoc(@RequestBody Map<String, Object> requestBody) {
-        try{
+    @RequestMapping(value = Api.READ_NODE_FROM_EMPLOYEE_DOC, method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<?> readNodeFromEmployeeDoc(@RequestBody Map<String, Object> requestBody) {
         JSONObject jsonBody = new JSONObject(requestBody).getJSONObject("body");
-        String response =  dataEmployeeService.getNodeFromeDoc(jsonBody);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-        }
-        catch(Exception ex){
-            String errorMessage;
-            errorMessage = "Error at API: hrm/pmis/get/v1/node-in-emp-doc, " + ex;
-            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
-        }
+        return dataEmployeeService.readNodeFromeDoc(jsonBody);
     }
 
     @PostMapping("/testing")
     @ResponseBody
-    public String testController(@RequestBody Map<String, Object> requestBody){
+    public String testController(@RequestBody Map<String, Object> requestBody) {
 
         /**
-         * {
-            "body"	 : {
-                "nodePath" : [
-                    "1", "5", "7"
-                ],
-                "input" : {
-                    "id" : "12" ,
-                    "value" : "12310231"
-                }
-            }
-        }
+         * { "body" : { "nodePath" : [ "1", "5", "7" ], "input" : { "id" : "12" ,
+         * "value" : "12310231" } } }
          */
 
         String jsonString = "{\"1\" : {\"2\" : {\"3\" : 4}    , \"5\" : {\"6\" : [\"q\", \"s\"], \"7\" : [{\"id\" : \"2\", \"value\" : \"200\"},{\"id\" : \"3\", \"value\" : \"300\"},{\"id\" : \"6\", \"value\" : \"300\"}]} }}";
@@ -106,7 +79,8 @@ public class DataEmployeeController {
         // jsonUtil.listRemoveNode("id", "6", destinationNode, nodePath);
         // log.warn(jsonUtil.getJsonNode(destinationNode, nodePath).toString());
         // log.warn(jsonUtil.getJsonArray(destinationNode, nodePath).toString());
-        // log.warn(jsonUtil.getNodeFromList("id", "6", destinationNode, nodePath).toString());
+        // log.warn(jsonUtil.getNodeFromList("id", "6", destinationNode,
+        // nodePath).toString());
 
         return destinationNode.toString();
     }
--- src/main/java/com/cokreates/grp/data/service/DataEmployeeService.java
+++ src/main/java/com/cokreates/grp/data/service/DataEmployeeService.java
@@ -6,6 +6,8 @@ import com.cokreates.grp.data.util.DataUtil;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -22,20 +24,29 @@ public class DataEmployeeService {
         Map<String, Object> queryResult = repository.getEmployee(requestParam);
         return dataUtil.mapToJsonObject(queryResult).toString();
     }
-    public String getNodeFromeDoc(JSONObject docObject){
-        String employeeOid = docObject.getString("employeeOid");
-        JSONArray nodePath = docObject.getJSONArray("nodePath");
-        
-        JSONObject employeeDoc = repository.getEmployeeDoc(employeeOid);
-        Object employeeMain = dataUtil.getNode(employeeDoc.getJSONObject("employee_main"), nodePath);
-        Object employeeTemp = dataUtil.getNode(employeeDoc.getJSONObject("employee_temp"), nodePath);
-        
-        JSONObject employeeBody = new JSONObject();
-        employeeBody.put("main", employeeMain);
-        employeeBody.put("temp", employeeTemp);
+    public ResponseEntity<?> readNodeFromeDoc(JSONObject docObject){
+        try {
+            String employeeOid = docObject.getString("employeeOid");
+            JSONArray nodePath = docObject.getJSONArray("nodePath");
+            
+            JSONObject employeeDoc = repository.getEmployeeDoc(employeeOid);
+            Object employeeMain = dataUtil.getNode(employeeDoc.getJSONObject("employee_main"), nodePath);
+            Object employeeTemp = dataUtil.getNode(employeeDoc.getJSONObject("employee_temp"), nodePath);
+            
+            JSONObject employeeBody = new JSONObject();
+            employeeBody.put("main", employeeMain);
+            employeeBody.put("temp", employeeTemp);
 
-        JSONObject resultObject = new JSONObject();
-        resultObject.put("body", employeeBody);
-        return resultObject.toString();
+            JSONObject resultObject = new JSONObject();
+            resultObject.put("body", employeeBody);
+            return new ResponseEntity<> (resultObject.toString(), HttpStatus.OK);
+            
+            
+        } catch (Exception ex) {
+            String errorMessage;
+            errorMessage = "Error at API: hrm/pmis/get/v1/node-in-emp-doc, " + ex;
+            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        }
+        
     }
 }
