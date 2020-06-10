065401eaf577bb0704864e5a81f0cfb237a620e3
Shariar076 <shariar1405076@gmail.com>
2020-06-10 17:22:22 +0600

    [resolved] conflict resolved

--- src/main/java/com/cokreates/grp/data/service/DataEmployeeService.java
+++ src/main/java/com/cokreates/grp/data/service/DataEmployeeService.java
@@ -567,6 +567,50 @@ public class DataEmployeeService {
         return responseObject;
     }
 
+    public ResponseEntity<?> updateNodeInListForRequest(JSONObject requestParameters){
+        JSONObject inputNode        = requestParameters.getJSONObject("node");
+        String nodeOid              = inputNode.getString("oid");
+        JSONArray nodePath          = requestParameters.getJSONArray("nodePath");
+        String employeeOid          = requestParameters.getString("employeeOid");
+        JSONObject requesterComment = requestParameters.getJSONObject("comment");
+
+        JSONObject employeeDoc      = null;
+
+        try {
+            employeeDoc = repository.getEmployee(requestParameters);
+        } catch (Exception ex) {
+            String errorMessage;
+            errorMessage = ex.toString();
+            return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+        }  
+
+        JSONObject mainDoc = employeeDoc.getJSONObject("employee_main");
+        JSONObject mainNode = (JSONObject) jsonUtil.getNodeFromList("oid", nodeOid, mainDoc, nodePath);
+
+        String queryNodeUpdate = dataHelper.updateEmpTempListInPmis(employeeDoc, nodePath, inputNode, employeeOid);
+        String queryApprovalHistoryInsert = dataHelper.approvalHistoryInsertWithComment(inputNode, mainNode, nodePath,
+                                                                                  employeeOid, requesterComment, "UPDATE_NODE_IN_LIST");
+        List<String> queryList = new ArrayList<>();
+        queryList.add(queryNodeUpdate);
+        queryList.add(queryApprovalHistoryInsert);
+
+        try {
+            repository.performTransaction(queryList);
+        } catch (Exception ex) {
+            String errorMessage;
+            errorMessage = ex.toString();
+            return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+        }     
+
+        JSONObject response = new JSONObject();
+        JSONObject responseBody = new JSONObject();
+        responseBody.put("oid", employeeOid);
+        response.put("body", responseBody); 
+        ResponseEntity<?> responseObject = new ResponseEntity<> (response.toString(), HttpStatus.OK);
+        
+        return responseObject;
+    }
+
 	public ResponseEntity<?> updateNodeEmployeeOffice(JSONObject requestParams) {
         JSONObject inputNode = requestParams.getJSONObject("node");
         String employeeOid   = requestParams.getString("employeeOid");
