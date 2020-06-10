b674424c6a4ce26611e122cd5f8f19d720decf57
dv66 <mostofamonsur9396@gmail.com>
2020-06-10 17:14:11 +0600

    [add] REMOVE_NODE_IN_LIST_FOR_REQUEST added to update APIs

--- src/main/java/com/cokreates/grp/data/controller/DataEmployeeController.java
+++ src/main/java/com/cokreates/grp/data/controller/DataEmployeeController.java
@@ -238,6 +238,17 @@ public class DataEmployeeController {
     }
 
 
+    @RequestMapping(value = Api.REMOVE_NODE_IN_LIST_FOR_REQUEST, 
+                    method = RequestMethod.POST, 
+                    consumes = {MediaType.APPLICATION_JSON_VALUE }, 
+                    produces = { MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<?> removeNodeInListForRequest(@RequestBody Map<String, Object> requestBody) {
+        JSONObject jsonBody = new JSONObject(requestBody).getJSONObject("body");
+        ResponseEntity<?> response = dataEmployeeService.removeNodeInListForRequest(jsonBody);
+        return response;
+    }
+
+
     
 
 
