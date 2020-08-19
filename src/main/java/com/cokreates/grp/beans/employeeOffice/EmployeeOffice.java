59d4648bc49dadcb89b3f2e53ba2cd535c193af8
Md. Mahbub Alam <mahbub@cokreates.com>
2020-08-19 14:13:23 +0600

    isAwardAdmin added in EmployeeOffice entity

--- src/main/java/com/cokreates/grp/beans/employeeOffice/EmployeeOffice.java
+++ src/main/java/com/cokreates/grp/beans/employeeOffice/EmployeeOffice.java
@@ -8,21 +8,20 @@ import lombok.EqualsAndHashCode;
 @Data
 public class EmployeeOffice extends BaseEntity {
 
-
-            private String employmentTypeOid;
-            private String inchargeLabelBn;
-            private String inchargeLabelEn;
-            private String isApprover;
-            private String isOfficeAdmin;
-            private String isOfficeHead;
-            private String isReviewer;
-            private String joiningDate;
-            private String lastOfficeDate;
-            private String officeOid;
-            private String officeUnitOid;
-            private String officeUnitPostOid;
-            private String status;
-            private String statusChangeDate;
-            private String systemRoleOid;
-
+    private String employmentTypeOid;
+    private String inchargeLabelBn;
+    private String inchargeLabelEn;
+    private String isApprover;
+    private String isOfficeAdmin;
+    private String isOfficeHead;
+    private String isReviewer;
+    private String joiningDate;
+    private String lastOfficeDate;
+    private String officeOid;
+    private String officeUnitOid;
+    private String officeUnitPostOid;
+    private String status;
+    private String statusChangeDate;
+    private String systemRoleOid;
+    private String isAwardAdmin;
 }
