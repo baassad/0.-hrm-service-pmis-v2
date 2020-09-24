f79fb478a3f962e7812e6d90771a7e189bc1d9df
antu <shubhashishkar@gmail.com>
2020-09-24 12:14:23 +0600

    [add] mobile banking validation added

--- src/main/java/com/cokreates/grp/beans/personal/mobileBanking/MobileBankingDTO.java
+++ src/main/java/com/cokreates/grp/beans/personal/mobileBanking/MobileBankingDTO.java
@@ -4,14 +4,21 @@ import com.cokreates.core.MasterDTO;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class MobileBankingDTO extends MasterDTO {
 
-
+    @NotBlank
     private String providerOid;
+
+    @NotBlank
     private String accountNumber;
     private String typeOfAccount;
+
+    @NotBlank
     private String nameOfAccount;
 }
