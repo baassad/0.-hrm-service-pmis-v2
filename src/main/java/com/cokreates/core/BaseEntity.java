915f8d662c8aefc5dd8704644cf1d610cb3a357e
imtiaz <imtiaz@cokreates.com>
2020-11-04 15:11:29 +0600

    ** create a new map table pmis+employee-office adjustment

--- src/main/java/com/cokreates/core/BaseEntity.java
+++ src/main/java/com/cokreates/core/BaseEntity.java
@@ -1,19 +1,16 @@
 package com.cokreates.core;
 
-import java.sql.Date;
-import java.sql.Timestamp;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @MappedSuperclass
