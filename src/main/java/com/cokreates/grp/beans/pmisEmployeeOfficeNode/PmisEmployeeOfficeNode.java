d0728e55513db25f1e914ead6327f3c6a68be744
imtiaz <imtiaz@cokreates.com>
2020-11-09 12:38:32 +0600

    ** update new column with default value

--- src/main/java/com/cokreates/grp/beans/pmisEmployeeOfficeNode/PmisEmployeeOfficeNode.java
+++ src/main/java/com/cokreates/grp/beans/pmisEmployeeOfficeNode/PmisEmployeeOfficeNode.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 
@@ -16,10 +17,20 @@ public class PmisEmployeeOfficeNode extends BaseEntity {
     private String pmisOid;
     private String employeeOfficeOid;
 
+    @Column(columnDefinition = "varchar(255) default 'No'")
     private String isApprover;
+
+    @Column(columnDefinition = "varchar(255) default 'No'")
     private String isReviewer;
+
+    @Column(columnDefinition = "varchar(255) default 'No'")
     private String isAttendanceAdmin;
+
+    @Column(columnDefinition = "varchar(255) default 'No'")
     private String isAttendanceDataEntryOperator;
+
+    @Column(columnDefinition = "varchar(255) default 'No'")
+    private String isAwardAdmin;
 
 
 
